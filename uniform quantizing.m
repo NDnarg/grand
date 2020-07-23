855b1f5b4cf5f7eb0dbc8e6f30254854d3f9044e
NDnarg <68454474+NDnarg@users.noreply.github.com>
2020-07-23 17:23:54 +0800

    Add files via upload

---
+++ uniform quantizing.m
@@ -0,0 +1,33 @@
+clear all;
+close all;
+clc;
+y=importdata('Dat_2.mat');
+figure;
+stem(y,'r');
+hold on
+
+n=16;
+del=(6-(-6))/n;
+for i=1:n
+    m(i)=-6+del*i;
+end
+Y=y;
+for i=1:10000
+    for j=1:n
+        if Y(i)>=6
+            Y(i)=6;
+        else if Y(i)<=-6
+                Y(i)=-6;
+            else if Y(i)>=m(j) && Y(i)<=m(j+1)
+                    Y(i)=(m(j)+m(j+1))/2;
+                   break
+                end
+            end
+        end
+    end
+end
+stem(Y,'b');
+title('uniform quantizing');
+hold off
+E=sum(abs(Y(i)-y(i))^2)
+            
